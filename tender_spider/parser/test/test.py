4ae87f2965c3026540b61174a9973b6c68abe15b
Biking0 <1342831695@qq.com>
2020-03-22 14:43:03 +0800

    delete idea

--- tender_spider/parser/test/test.py
+++ tender_spider/parser/test/test.py
@@ -9,12 +9,12 @@ from tender_spider.get_data.test.get_data import get_data
 import time
 
 # 获取简略信息
-for i in range(424, 600):
+for i in range(1, 611):
     url = 'http://www.hngp.gov.cn/henan/list2?channelCode=0101&pageSize=16&bz=1&gglx=0&pageNo='+str(i)
     print('#############',i)
     result_data = get_data(url)
     sample_parser.parser(result_data)
-    time.sleep(5)
+    time.sleep(6)
 
 # 获取详细信息
 # url = 'http://www.hngp.gov.cn/henan/content?infoId=423717&channelCode=H600101'

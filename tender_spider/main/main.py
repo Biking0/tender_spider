9f6de4832c7fa8e342339b69b87571df56f63a80
Biking0 <1342831695@qq.com>
2020-03-23 13:06:12 +0800

    test commit

--- tender_spider/main/main.py
+++ tender_spider/main/main.py
@@ -3,4 +3,26 @@
 # by hyn
 # 20200322
 
-# 111
+
+# 构造url，根据url获取书数据，调用简略解析，调用详细解析
+from tender_spider.url_pool import url_pool  as url_pool
+import tender_spider.config as config
+from tender_spider.get_data.test.get_data import get_data
+import tender_spider.parser.test.sample_parser as sample_parser
+import tender_spider.parser.test.content_parser as content_parser
+
+
+# 构造url，根据url获取书数据，调用简略解析，调用详细解析
+def main():
+    pro_totalpage=url_pool.pro_totalpage()
+    city_totalpage=url_pool.city_totalpage()
+
+    print('开始爬取，总页数：',pro_totalpage)
+    for i in range(1,pro_totalpage):
+        url=config.pro_page_url+str(i)
+        print('正在爬取，第',i,'页',url)
+        response=get_data(url)
+        sample_parser.parser(response)
+
+
+
---
+++ tender_spider/start.py
@@ -0,0 +1,10 @@
+# encoding=utf8
+# 爬虫启动器
+# by hyn
+# 20200322
+
+
+from tender_spider.main import main
+
+if __name__ == "__main__":
+    main.main()
